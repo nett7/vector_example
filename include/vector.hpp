424763c7f7899a3f461a47bcfddc2ecd952a7e05
Nett <37066846+nett7@users.noreply.github.com>
2018-03-28 23:09:52 +0300

    Update vector.hpp

--- include/vector.hpp
+++ include/vector.hpp
@@ -1,27 +1,32 @@
+#pragma once
 #include <iostream>
+
 
 class vector_t
 {
 private:
-	int * elements_;
-	std::size_t size_;
-	std::size_t capacity_;
+    int * elements_;
+    std::size_t size_;
+    std::size_t capacity_;
+
+    void extend_capacity_();
+    void decrease_capacity();
 public:
-	vector_t();
-	vector_t(vector_t const & other);
-	vector_t & operator =(vector_t const & other);
-	~vector_t();
+    vector_t();
+    vector_t(vector_t const & other);
+    vector_t & operator =(vector_t const & other);
+    ~vector_t();
 
-	std::size_t size() const;
-	std::size_t capacity() const;
+    std::size_t size() const;
+    std::size_t capacity() const;
 
-	void push_back(int value);
-	void pop_back();
+    void push_back(int value);
+    void pop_back();
 
-	int & operator [](std::size_t index);
-	int operator [](std::size_t index) const;
+    int & operator [](std::size_t index);
+    int operator [](std::size_t index) const;
 
-	bool operator ==(vector_t const & other) const;
+    bool operator ==(vector_t const & other) const;
 };
 
 bool operator !=(vector_t const & lhs, vector_t const & rhs);
