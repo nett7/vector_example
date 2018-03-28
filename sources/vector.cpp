7f30bca7e20e0d8f39ca9b18543e6264b76374ac
Nett <37066846+nett7@users.noreply.github.com>
2018-03-28 23:09:02 +0300

    Update vector.cpp

--- sources/vector.cpp
+++ sources/vector.cpp
@@ -1,59 +1,116 @@
+#include "vector.hpp"
 #include <algorithm>
 #include <cassert>
+#include <memory>
 
-#include "vector.hpp"
 
-vector_t::vector_t()
-{
-}//erf,erf,e
-
-vector_t::vector_t(vector_t const & other)
-{
+vector_t::vector_t() {
+    elements_ = nullptr;
+    size_ = 0;
+    capacity_ = 0;
 }
 
-vector_t & vector_t::operator =(vector_t const & other)
-{
-	return *this;
+vector_t::vector_t(vector_t const &other) {
 }
 
-bool vector_t::operator ==(vector_t const & other) const
-{
-	return false;
+vector_t &vector_t::operator=(vector_t const &other) {
+    size_ = other.size_;
+    capacity_ = other.capacity_;
+    if (elements_ != nullptr) {
+        delete[] elements_;
+    }
+    elements_ = new int[other.capacity_];
+    std::copy(&other.elements_[0], &other.elements_[other.size_], elements_);
+
+    return *this;
 }
 
-vector_t::~vector_t()
-{
+bool vector_t::operator==(vector_t const &other) const {
+    for (int i = 0; i < other.size_; i++) {
+        if (elements_[i] != other[i]) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
-std::size_t vector_t::size() const
-{
-    return 0;
+vector_t::~vector_t() {
+    delete []elements_;
 }
 
-std::size_t vector_t::capacity() const
-{
-    return 0;
+std::size_t vector_t::size() const {
+    return size_;
 }
 
-void vector_t::push_back(int value)
-{
+std::size_t vector_t::capacity() const {
+    return capacity_;
 }
 
-void vector_t::pop_back()
-{
+void vector_t::push_back(int value) {
+    if (size_ == capacity_) {
+        extend_capacity_();
+    }
+    elements_[size_] = value;
+    size_++;
 }
 
-int & vector_t::operator [](std::size_t index)
-{
-	return elements_[0];
+void vector_t::pop_back() {
+    size_--;
+    if(size_< (capacity_/2)) {
+        decrease_capacity();
+    }
+        int *temp = elements_;
+        elements_ = new int[capacity_];
+        std::copy(temp, &temp[size_-1], elements_);
+        delete[]temp;
+    
 }
 
-int vector_t::operator [](std::size_t index) const
-{
-	return 0;
+int &vector_t::operator[](std::size_t index) {
+    return elements_[index];
 }
 
-bool operator !=(vector_t const & lhs, vector_t const & rhs)
-{
-	return true;
+int vector_t::operator[](std::size_t index) const {
+    return elements_[index];
+}
+
+void vector_t::extend_capacity_() {
+    if (elements_ == nullptr) {
+        elements_ = new int[1];
+        capacity_ = 1;
+    } else {
+        int *temp = elements_;
+        capacity_ *= 2;
+        elements_ = new int[capacity_];
+        std::copy(temp, &temp[size_], elements_);
+        delete[]temp;
+
+
+    }
+}
+
+void vector_t::decrease_capacity() {
+    if (capacity_ > 1) {
+        int *temp = elements_;
+        capacity_ /= 2;
+        elements_ = new int[capacity_];
+        std::copy(temp, &temp[size_], elements_);
+        delete[]temp;
+    }
+
+}
+
+bool operator!=(vector_t const &lhs, vector_t const &rhs) {
+    if(lhs.size_!=rhs.size_){
+        return true;
+    }
+    else {
+    for (int i = 0; i < lhs.size_; i++) {
+        if (lhs[i] != lhs[i]) {
+            return true;
+        }}
+    }
+
+    return false;
 }
